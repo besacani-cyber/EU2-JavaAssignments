67a233dbe95e72f5303df9c2de119a5c6816845c
ismailsinansahin <59430934+ismailsinansahin@users.noreply.github.com>
2020-04-27 18:23:06 +0300

    Add files via upload

--- automation_assignment_02/Task_09_10_11_12_with_DataProvider.java
+++ automation_assignment_02/Task_09_10_11_12_with_DataProvider.java
@@ -24,24 +24,35 @@ public class Task_09_10_11_12_with_DataProvider {
 
     @DataProvider (name = "data")
     public Object[][] data(){
+
         driver.get("https://practice-cybertekschool.herokuapp.com/status_codes");
+
+        //takes the status codes links as a String and collects in a list
         List<WebElement> allCodes = driver.findElements(By.xpath("//div/ul/li"));
+
+        //create an object to keep the status codes and return them one by one to the test method
         Object[][] statusCodes = new String[allCodes.size()][1];
         for (int i = 0; i < allCodes.size(); i++) {
             statusCodes[i][0] = allCodes.get(i).getText();
         }
+        
         return statusCodes;
+
     }
 
     @Test (dataProvider = "data")
     public void testCase_09_10_11_12(String statusCode) {
+
+        //takes status code from data provider as a String and creates new paths to click the links
         String xPath = "//a[.='" + statusCode + "']";
         driver.findElement(By.xpath(xPath)).click();
 
+        //verifying the message on the screen contains the correct status code
         String actualResult = driver.findElement(By.xpath("//p")).getText();
         String expectedResult = "This page returned a " + statusCode + " status code";
         Assert.assertTrue(actualResult.contains(expectedResult));
 
+        //turns back to the status code page to go on with the other codes links
         driver.navigate().back();
 
     }
